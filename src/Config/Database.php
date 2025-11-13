2a24ec58aaf83f9a633f3bd9dce0a40753f86475
Wesley AV <wesley.contato@hotmail.com>
2025-11-12 22:01:48 -0300

    Corrige variáveis de ambiente

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -12,11 +12,11 @@ class Database
     public static function getConnection()
     {
         if (self::$pdo === null) {
-            $host = getenv('MYSQLHOST');
-            $port = getenv('MYSQLPORT');
-            $db = getenv('MYSQL_DATABASE');
-            $user = getenv('MYSQLUSER');
-            $pass = getenv('MYSQLPASSWORD');
+            $host = getenv('DB_HOST');
+            $port = getenv('DB_PORT');
+            $db = getenv('DB_NAME');
+            $user = getenv('DB_USER');
+            $pass = getenv('DB_PASSWORD');
             $charset = 'utf8mb4';
             $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
